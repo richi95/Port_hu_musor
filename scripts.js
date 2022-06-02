afac40200f503db0c98b137cdd4e625dafcf584b
richi95 <richi950903@gmail.com>
2022-06-02 08:20:40 +0200

    port tv

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 //Az aktuális URL-t paraméterezi a kiválasztott dátummal 
 const date = document.getElementById("date");
-let url = "http://localhost/webprog/php/portmusor2/index.php";
+let url = window.location.href;
 
 date.addEventListener("change", function (e) {
   window.location.href = url + "?date=" + e.target.value;
